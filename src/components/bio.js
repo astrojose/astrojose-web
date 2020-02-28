2a9d6a0245d0ba645421a62e16dab20d47552885
Joseph Emmanuel <jossey1918@gmail.com>
2020-02-28 13:22:29 +0300

    update favicon

--- src/components/bio.js
+++ src/components/bio.js
@@ -29,6 +29,7 @@ const Bio = () => {
           }
           social {
             twitter
+            instagram
           }
         }
       }
@@ -59,8 +60,8 @@ const Bio = () => {
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+        <a href={`https://instagram.com/${social.instagram}`}>
+          ig @astrojose.js
         </a>
       </p>
     </div>
