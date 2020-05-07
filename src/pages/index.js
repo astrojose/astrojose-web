611824e47a6d17f50701a9b4bb9e2245116c2ed3
Joseph Emmanuel <jossey1918@gmail.com>
2020-05-07 08:19:57 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="astrojose's site" />
       <Bio /> 
       <h2 style={{width:300, margin:`auto`}}>Latest Articles</h2>
       {posts.map(({ node }) => {
