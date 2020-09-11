c2345ffa50399bdfb9f29d7a657ae19b02e606d5
Joseph Emmanuel <jossey1918@gmail.com>
2020-09-12 00:48:11 +0300

    Update newsletter.js

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
- const Newsletter = () =>{
-     return <div style={{ width: 480, margin:`auto`}}>
-            <iframe title="My News Letter" 
-            src="https://astrojose.substack.com/embed" width="480" height="320" 
-            style={{ border:`1px solid #EEE`, background:`white`} } frameborder="0" scrolling="no"></iframe>
-        </div>
- }
+//  const Newsletter = () =>{
+//      return <div style={{ width: 480, margin:`auto`}}>
+//             <iframe title="My News Letter" 
+//             src="https://astrojose.substack.com/embed" width="480" height="320" 
+//             style={{ border:`1px solid #EEE`, background:`white`} } frameborder="0" scrolling="no"></iframe>
+//         </div>
+//  }
 
-export default Newsletter
+// export default Newsletter
