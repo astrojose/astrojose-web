8e9c6d455c7406bec1ffe8672cd0e511a101baaf
astrojose <jossey1918@gmail.com>
2021-06-03 10:00:49 +0300

    refac: create footer content component:w

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
-import NavLink from './NavLink'
 import NavBar from './navbar'
-import { rhythm, scale } from "../utils/typography"
+import Footer from './footer'
+import Helmet from "react-helmet"
 import "./../style.css" 
 
 const Layout = ({ location, title, children }) => {
@@ -10,22 +9,14 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <>
-    <header className='main-wrapper'>
-      	<NavBar />
-	  </header>
-    <main className="main-wrapper">{children}</main>
+    <Helmet>
+      <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+    </Helmet>
 
-    <footer className="main-wrapper">	
-      <div className="social-links">
-        <a href="https://instagram.com/astrojose_" 
-        className="footer-link" target="_blank">instagram </a>
-        <a href="https://twitter.com/astrojose_" className="footer-link" target="_blank">twitter </a>
-        <a href="https://astrojose.substack.com/subscribe" className="footer-link" target="_blank">get newsletter </a> <br/>
-      </div>
-      <div className="copyright">
-        © {new Date().getFullYear()}, <a href="/" >astrojose</a> 
-      </div>
-    </footer>
+    <header className='main-wrapper'><NavBar /> </header>
+    <main className="main-wrapper">{children}</main>
+    <footer className="main-wrapper">	<Footer /> </footer>
+
     </>
   )
 }
