118d1757d230274f9b6f65acc1279bbe1466754d
astrojose <jossey1918@gmail.com>
2021-06-03 08:49:13 +0300

    fix: good mobile responsive

--- src/components/category-header.js
+++ src/components/category-header.js
@@ -4,7 +4,7 @@ const CategoryHeader = (props) => {
   return (
     <div class="category-header">
       <div className='title'>{props.title}</div>
-      <div className="more"><a id="a-link"  href="/blog"> See More ... </a></div>
+      <div className="more"><a id="a-link"  href="/blog"> See More</a></div>
     </div>
   )
 } 
